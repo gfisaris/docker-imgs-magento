bbdd35c62d661882ad6c0f60993295746807d004
gfisaris <gfisaris@gmail.com>
2016-11-07 12:48:18 +0000

    Circle CI Integration for automatic build n push of the docker image to ecr

---
+++ circle-ci/push-dckimg2ecr.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
+
+configure_aws_cli(){
+	aws --version
+	aws configure set default.region eu-central-1
+	aws configure set default.output json
+}
+
+push-dckimg2ecr(){
+	eval $(aws ecr get-login --region us-east-1)
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/my_app:$CIRCLE_SHA1
+}
+
+configure_aws_cli
+push-dckimg2ecr
