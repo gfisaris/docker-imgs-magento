de3c6766388291a3e5abf79ec4e7f3bb4ac07c4a
gfisaris <gfisaris@gmail.com>
2016-11-07 17:27:06 +0000

    Fix sed escaping $ character

--- circle-ci/update-ecs.sh
+++ circle-ci/update-ecs.sh
@@ -23,8 +23,8 @@ gitpush-iac_aws_ecs() {
 create-iac_aws_ecs_taskdef() {
   cd ~/deployment.ws/iac-terraform-aws-ecs
   cp tasks/lep_stack.json.tpl tasks/lep_stack-$CIRCLE_SHA1.json
-  sed -i 's/DCK_IMG_NAME/my_app/g' tasks/lep_stack-$CIRCLE_SHA1.json
-  sed -i 's/DCK_IMG_TAG/$CIRCLE_SHA1/g' tasks/lep_stack-$CIRCLE_SHA1.json
+  sed -i "s/DCK_IMG_NAME/my_app/g" tasks/lep_stack-$CIRCLE_SHA1.json
+  sed -i "s/DCK_IMG_TAG/$CIRCLE_SHA1/g" tasks/lep_stack-$CIRCLE_SHA1.json
 }
 
 gitclone-iac_aws_ecs
