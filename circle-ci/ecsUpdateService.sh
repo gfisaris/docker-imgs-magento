b93354d5c8ee7c987c5f8ddb0ba9000461b80986
gfisaris <gfisaris@gmail.com>
2017-01-06 16:19:38 +0000

    Add CircleCI Scripts (ecsUpdateService, push2ecr)

---
+++ circle-ci/ecsUpdateService.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+ecsRegion=$1
+ecsCluster=$2
+ecsService=$3
+ecsTaskDef=$4
+
+ecsOldTaskD=$(aws ecs describe-services --region $ecsRegion --cluster $ecsCluster --service $ecsService | jq -r '.services[] | .taskDefinition')
+
+aws ecr get-login --region $ecsRegion || exit 1
+aws ecs describe-services --region $ecsRegion --cluster $ecsCluster --service $ecsService || exit 1
+
+aws ecs update-service --region $ecsRegion --cluster $ecsCluster --service $ecsService --task-definition $ecsTaskDef || exit 1
+  echo "Service TaskDefinition Update has started!"
+  echo "Current TaskDefinition:" $ecsOldTaskD
+  echo "Deploying TaskDefinition:" $ecsTaskDef
+
+getDesiredCount="aws ecs describe-services --region $ecsRegion --cluster $ecsCluster --service $ecsService | jq '.services[].deployments[] | select(.taskDefinition | contains (\"$ecsTaskDef\")) | .desiredCount'"
+getRunningCount="aws ecs describe-services --region $ecsRegion --cluster $ecsCluster --service $ecsService | jq '.services[].deployments[] | select(.taskDefinition | contains (\"$ecsTaskDef\")) | .runningCount'"
+
+if [[ $(eval $getDesiredCount) = 0 ]];
+  then
+    echo "Service Desired Count is set to 0." && exit 1
+fi
+
+  while [ $(eval $getDesiredCount) != $(eval $getRunningCount) ];
+    do
+      echo "Service Tasks Desired Count:" $(eval $getDesiredCount)
+      echo "Updated Tasks Running Count:" $(eval $getRunningCount)
+      echo "Service TaskDefinition Update is still in progress.."; sleep 5;
+  done
+  echo "Service TaskDefinition Update has successfully completed!"
