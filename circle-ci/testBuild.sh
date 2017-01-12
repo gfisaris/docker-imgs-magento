7b0cda439260e2db97c8d2b9280d245e013ffa13
gfisaris <gfisaris@gmail.com>
2017-01-12 15:51:44 +0000

    Fix Docker-Compose not running

--- circle-ci/testBuild.sh
+++ circle-ci/testBuild.sh
@@ -6,6 +6,6 @@ eval $(aws ecr get-login --region $CI_AWS_ACTIVE_REGION)
 
 docker-compose up -d
 
-curl --retry 10 --retry-delay 5 127.0.0.1:$(docker-compose port $CI_AWS_ECR_REPO 80 | cut -d ":" -f "2")/fpm_ping | grep "pingpong"
+curl --retry 10 --retry-delay 5 127.0.0.1:$(docker-compose port $CI_AWS_ECR_REPO\_nginx 80 | cut -d ":" -f "2")/fpm_ping | grep "pingpong"
 
 docker stop $(docker ps -a -q)
