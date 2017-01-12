7eb29f5a31258598871c2287807a596a68fbbd79
gfisaris <gfisaris@gmail.com>
2017-01-12 14:55:38 +0000

    Move docker-compose.yml to base folder

--- circle-ci/ecsCLiCompose.sh
+++ circle-ci/ecsCLiCompose.sh
@@ -4,4 +4,4 @@ set -e
 AWS_ECS_SERVICE_NAME=$CI_PROJECT_NAME"_"$PRJ_ENV
 
 ecs-cli configure --profile default --region $CI_AWS_ACTIVE_REGION --cluster $CI_AWS_ECS_CLUSTER --compose-project-name-prefix $AWS_ECS_SERVICE_NAME || exit 1
-ecs-cli compose --project-name $CIRCLE_BUILD_NUM --file ecs-DockerCompose.yml create || exit 1
+ecs-cli compose --project-name $CIRCLE_BUILD_NUM --file docker-compose.yml create || exit 1
