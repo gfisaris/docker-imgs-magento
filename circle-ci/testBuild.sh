7ca46d0ff3500b6b44611be41d149fb0de9899fe
gfisaris <gfisaris@gmail.com>
2017-01-13 09:02:00 +0000

    Modify Local/CI Build-Test script

--- circle-ci/testBuild.sh
+++ circle-ci/testBuild.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-sudo pip install docker-compose
 eval $(aws ecr get-login --region $CI_AWS_ACTIVE_REGION)
 
+sudo pip install docker-compose
 docker-compose up -d
 
 curl --retry 10 --retry-delay 5 127.0.0.1:$(docker-compose port $CI_PROJECT_NAME\_nginx 80 | cut -d ":" -f "2")/fpm_ping | grep "pingpong"
